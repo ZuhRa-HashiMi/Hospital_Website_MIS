3c4faac5332e69771a7169d6f10b065bca012280
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-07-20 00:22:42 +0430

    Remove require connection

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php require_once("header.php");?>
+
+        <h2>Welcome to our website </h2>
+        <p><span>Simply dummy text of the printing and typesetting industry.</span></p>
+        <div class="bg"></div>
+        <p>Lorem Ipsum has been the industry's standard dummy text ever since thes, when an unknown printer.<a href="#">Simply dummy text of the printing and typesetting industry</a>. Lorem Ipsum has been the industry's standard dummy text ever since thes, when an unknown printer.Simply dummy text of the printing and typesetting industry. <br />
+          Lorem Ipsum has been the industry's standard dummy text ever since thes, when an unknown printer.</p>
+        <div class="bg"></div>
+        <h2>Opening Title</h2>
+        <p>Dummy text of the printing and typesetting industry.</p>
+        <p>Been the industry's standard dummy text ever since thes, when rinter.Simply dummy text of the printing and <a href="#">typesetting industry. Lorem Ipsum </a>has been the industry's standard dummy text ever since thes, when an unknown printer.Simply dummy text of the printing and typesetting industry. </p>
+        <div class="bg"></div>
+        <h2>What we offer</h2>
+        <p>Been the industry's standard dummy text ever since thes, when rinter.Simply dummy text orem Ipsum has  standard dummy text ever since thes, when an unknown printer.Simply dummy text of the printing and typesetting industry. </p>
+        <p>&nbsp;</p>
+        <ul>
+          <li>Been the industry's standard dummy text ever since thes, when rinter.</li>
+          <li>Simply dummy text orem Ipsum has  standard dummy text ever since thes, when an unknown. </li>
+          <li>Undustry's standard dummy text ever since thes, when rinter.</li>
+        </ul>
