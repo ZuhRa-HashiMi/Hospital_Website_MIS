80e5cf34b8a95c766d58355c2e96436efb440b88
SodabaEhsan <87577617+SodabaEhsan@users.noreply.github.com>
2022-07-20 09:20:17 +0430

    cut all footer section from index file
    because of master page we have to create more than one page

---
+++ footer_website.php
@@ -0,0 +1,47 @@
+ </div>
+      <div class="Latest col-lg-4 col-md-4 col-sm-4 col-xs-12">
+        <h2>News</h2>
+        <p><img src="images/img_1.jpg" width="307" height="130" alt="img" /></p>
+        <p><a href="#"><strong>Dummy text of the printing</strong></a></p>
+		<h2>ADS</h2>
+        <p>Been the industry's standard dummy text ever since thes, when rinter.Simply dummy text of thorem Ipsum has been the industry's standard dummy text.</p>
+        <p><img src="images/img_2.jpg" width="307" height="130" alt="img" /></p>
+        <p><a href="#"><strong>Text of the printing</strong></a></p>
+        <p>Undustry's standard dummy text ever since thes, when rinter.Simply dummy text of thorem Ipsum has been the industry's standard dummy text.</p>
+      </div>
+      <div class="clr"></div>
+    </div>
+  </div>
+  <div class="FBG">
+    <div class="FBG_resize">
+      <div class="Testimonials  col-lg-4 col-md-4 col-sm-4 col-xs-12">
+        <h2>Testimonials </h2>
+        <p><em>“Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the. industry's standard dummy”</em></p>
+        <p>&nbsp;</p>
+        <p><a href="#">-Jhon Jhohonson, web designer</a></p>
+      </div>
+      <div class="Company col-lg-4 col-md-4 col-sm-4 col-xs-12">
+        <h2>Company News</h2>
+        <p class="data">03.12.09</p>
+        <p>&nbsp;</p>
+        <p>Lorem Ipsum is simply dummy text of printing and typesetting industry...</p>
+        <p>&nbsp;</p>
+      </div>
+      <div class="Con col-lg-4 col-md-4 col-sm-4 col-xs-12">
+        <h2>Contact us</h2>
+        <p><strong>Name of the Company</strong><br />
+          2901 Marmora Road, Glassgow, D04 59GR<br />
+          Telephone: +1 234 567 8910</p>
+        <p>FAX: +1 234 567 8910<br />
+          E-mail: <a href="#">mail@yoursitename.com</a></p>
+      </div>
+      <div class="clr"></div>
+    </div>
+  </div>
+  <div class="footer">
+    <p>Copyright © Sitename.com. <a href="http://dreamtemplate.com/">dreamtemplate.com</a>. All Rights Reserved<br />
+      <a href="index.html">Home</a> | <a href="contact.html">Contact</a> | <a href="blog.html">RSS</a></p>
+  </div>
+</div>
+</body>
+</html>
