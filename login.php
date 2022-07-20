8a71348138c8df8216797709a53f73beef671ace
SodabaEhsan <87577617+SodabaEhsan@users.noreply.github.com>
2022-07-20 09:32:59 +0430

    add bootstrap fuction for style the class

--- login.php
+++ login.php
@@ -1,4 +1,29 @@
-<form method="post">
+<div class="panel-heading text-center">
+			<h1>Login to MIS</h1>
+		</div>
+	
+		<div class="panel-body">
+		
+			<?php if(isset($_GET["login"])) { ?>
+				<div class="alert alert-danger">
+					Incorrect Username or Password
+				</div>
+			<?php } ?>
+			
+			<?php if(isset($_GET["logout"])) { ?>
+				<div class="alert alert-success">
+					You are successfully logged out!
+				</div>
+			<?php } ?>
+			
+			<?php if(isset($_GET["notlogin"])) { ?>
+				<div class="alert alert-warning">
+					Please login first!
+				</div>
+			<?php } ?>                     
+
+
+                          <form method="post">
 				
 				<div class="input-group">
 					<span class="input-group-addon">
