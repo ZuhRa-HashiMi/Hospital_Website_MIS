b7d89197e90ff59ab0ea8e45711aee5e3e2d0895
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-07-20 21:28:46 +0430

    Add getValue function

--- connection.php
+++ connection.php
@@ -1,8 +1,15 @@
 <?php 
   $con = mysqli_connect("localhost", "root", "");
 	mysqli_select_db($con, "script");
-  if (!isset($_SESSION)) {
+    if (!isset($_SESSION)) {
 	   session_start();
 	   }
+	   
+
+	   
+    function getValue($value) {
+		global $con;
+		return mysqli_real_escape_string($con, $value);
+	}
 	
   ?>
