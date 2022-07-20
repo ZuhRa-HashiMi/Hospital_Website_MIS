b92fce5dc1bcc008c21199692e54c70b1bb3a546
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-07-20 17:03:51 +0430

    created login form

--- login.php
+++ login.php
@@ -1,54 +1,37 @@
-<?php
-
-	require_once("connection.php");
-
-	if(isset($_SESSION["user_id"])) {
-		header("Location:home.php");
-	}
-	
-	
-	if(isset($_POST["username"])) {
-		$username =($_POST["username"]);
-		$password =($_POST["password"]);
-		
+ <?php 
+ 
+    	require_once("connection.php");
+        if(isset($_POST["username"])) {
+		$username = ($_POST["username"]);
+		$password = ($_POST["password"]);
 		$result = mysqli_query($con, "SELECT * FROM users WHERE username = '$username' AND password = PASSWORD('$password') ");
-		
 		if(mysqli_num_rows($result) == 1) {
 			$row_result = mysqli_fetch_assoc($result);
 			$_SESSION["user_id"] = $row_result["user_id"];
 			
-			$_SESSION["user_type"] = $row_result["user_type"];
-			$_SESSION["admin_level"] = $row_result["admin_level"];
-			$_SESSION["website_level"] = $row_result["website_level"];
-			$_SESSION["stock_level"] = $row_result["stock_level"];
-			$_SESSION["hr_level"] = $row_result["hr_level"];
-			$_SESSION["finance_level"] = $row_result["finance_level"];
-			$_SESSION["surgery_level"] = $row_result["surgery_level"];
-			$_SESSION["pharmacy_level"] = $row_result["pharmacy_level"];
-			$_SESSION["laboratoar_level"] = $row_result["laboratoar_level"];
-			$_SESSION["blood_bank_level"] = $row_result["blood_bank_level"];
-			$_SESSION["patient_level"] = $row_result["patient_level"];
-			
 			header("location:home.php");
-		}
-		else {
+			
+			}
+			else {
 			header("location:login.php?login=failed");
 		}
 		
-	}
-
-?>  
+		
+		}
+		
+		?>
  
+ 
+ 
+ <?php require_once("header.php");?>
+              
 
+	         
+	<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 col-lg-offset-3 col-md-offset-3 col-sm-offset-3 col-xs-offset-0">
 
-<?php require_once("header.php"); ?>
-
-<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 col-lg-offset-3 col-md-offset-3 col-sm-offset-3 col-xs-offset-0">
-
-	<div class="panel panel-primary">      
-
-
-<div class="panel-heading text-center">
+	<div class="panel panel-primary">
+	
+		<div class="panel-heading text-center">
 			<h1>Login to MIS</h1>
 		</div>
 	
@@ -59,21 +42,7 @@
 					Incorrect Username or Password
 				</div>
 			<?php } ?>
-			
-			<?php if(isset($_GET["logout"])) { ?>
-				<div class="alert alert-success">
-					You are successfully logged out!
-				</div>
-			<?php } ?>
-			
-			<?php if(isset($_GET["notlogin"])) { ?>
-				<div class="alert alert-warning">
-					Please login first!
-				</div>
-			<?php } ?>                     
-
-
-                          <form method="post">
+                <form method="post">
 				
 				<div class="input-group">
 					<span class="input-group-addon">
@@ -92,7 +61,7 @@
 				<input type="submit" value="Login" class="btn btn-primary">
 				
 			</form>
-			
+	
 		</div>
 	
 	</div>
@@ -100,4 +69,5 @@
 	</div>
 
 
-<?php require_once("footer_website.php"); ?>
+<?php require_once("footer_website.php");?>
+
