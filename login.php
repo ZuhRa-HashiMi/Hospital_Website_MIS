2fd2c0407cf14db8d4ea9f2cb0544afb246e4d00
SodabaEhsan <87577617+SodabaEhsan@users.noreply.github.com>
2022-07-20 09:30:23 +0430

    created form section

---
+++ login.php
@@ -0,0 +1,19 @@
+<form method="post">
+				
+				<div class="input-group">
+					<span class="input-group-addon">
+						Username:
+					</span>
+					<input type="text" class="form-control" name="username">
+				</div>
+				
+				<div class="input-group">
+					<span class="input-group-addon">
+						Password:
+					</span>
+					<input type="password" class="form-control" name="password">
+				</div>
+				
+				<input type="submit" value="Login" class="btn btn-primary">
+				
+			</form>
