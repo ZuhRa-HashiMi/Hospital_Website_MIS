019a449623a8f8ee8b9351a54a46317155f3a8d4
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-07-20 22:27:52 +0430

    add department form and bootstrap class

---
+++ department_add.php
@@ -0,0 +1,35 @@
+<?php require_once("header.php");?>
+
+
+
+<div class="col-lg-8 col-md-8 col-ms-8 col-xs-12 col-lg-offset-2 col-md-offset-2 col-sm-offset-2 col-xs-offset-0">
+
+
+<div class="panel panel-primary">
+	
+	<div class="panel-heading">
+		<h1>Add New Department</h1>
+	</div>
+
+	<div class="panel-body">
+	
+		<form method="post">
+			
+			<div class="input-group">
+				<span class="input-group-addon">
+					Department Name:
+				</span>
+				<input type="text" class="form-control" name="department_name">
+			</div>
+			
+			<input type="submit" class="btn btn-primary" value="Add Department">
+			
+		</form>
+		</div>
+		
+	</div>
+
+</div>
+<?php require_once("footer_mis.php");?>
+
+
