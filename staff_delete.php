4274e96a1aedcdb8b51f5d074ffad5ee549a9651
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-07-21 20:24:49 +0430

    crreated staff_delete file

---
+++ staff_delete.php
@@ -0,0 +1,24 @@
+<?php
+
+     require_once("connection.php");
+	 
+	$staff_id = getValue($_GET["staff_id"]);
+	
+	$staff = mysqli_query($con, "SELECT photo FROM staff WHERE staff_id = $staff_id");
+	$row_staff = mysqli_fetch_assoc($staff);
+	
+	$result = mysqli_query($con, "DELETE FROM staff WHERE staff_id = $staff_id");
+	
+	if($result) {
+		if($row_staff["photo"] != "images/staff/user_m.png" && $row_staff["photo"] != "images/staff/user_f.png") {
+			unlink($row_staff["photo"]);
+		}
+		
+		header("location:staff_list.php?delete=done");
+	}
+	else {
+		header("location:staff_list.php?error=notdelete");
+	}
+
+
+?>
