940e74ea67977b4fd6573913a154064e88d4dbc1
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-07-21 21:52:16 +0430

    created attendance file for employee

--- attendance_list.php
+++ attendance_list.php
@@ -1,20 +1,35 @@
 <?php require_once("connection.php"); ?>
-<?php 
-  $attendance = mysqli_query($con, "SELECT * FROM attendances");
+<?php
+
+	if(isset($_GET["year"])) {
+		$year = getValue($_GET["year"]);
+		$month = getValue($_GET["month"]);
+	}
+	else { 
+		$year = date("Y");
+		$month = date("m");
+	}
+
+	$attendance = mysqli_query($con, "SELECT *, (SELECT SUM(hours) FROM attendances WHERE staff_id = staff.staff_id AND absent_month = $month AND absent_year = $year) AS totalhours FROM staff");
 	$row_attendance = mysqli_fetch_assoc($attendance);
-	
-	
-	
-	
-	
-	
+
 ?>
-
-
 <?php require_once("header.php"); ?>
 
+<a style="margin-left:10px;" href="#" id="print" class="noprint btn btn-primary pull-right">
+	<span class="glyphicon glyphicon-print"></span> 
+	Print
+</a>
 
-<h2>Attendace Report</h2>
+<a href="attendance_add.php" class="noprint btn btn-primary pull-right">
+	<span class="glyphicon glyphicon-plus"></span>
+	Add Absent
+</a>
+
+
+
+<h2>Attendance Report (<?php echo $year; ?>/<?php echo $month; ?>)</h2>
+
 <?php if(isset($_GET["add"])) { ?>
 	<div class="alert alert-success alert-dismissable">
 		<button class="close" data-dismiss="alert" area-hidden="true">&times;</button>
@@ -42,57 +57,83 @@
 		Could not delete selected attendance!
 	</div>
 <?php } ?>
-<table class="table table-striped">
-  <tr>
-     <th>ID</th>
-	 <th>attendance Name</th>
-	  <th>Photo</th>
-	   <th>Position</th>
-	    <th>Salary</th>
-			<th>attendance Type</th>
-		 <th>Department</th>
-		 <th class="noprint">Edit</th>
-		<th class="noprint">Delete</th>
-  </tr>
-  <?php do{?>
-  <tr>
-       <td><?php echo $row_attendance["attendance_id"];?></td>
-       <td><?php echo $row_attendance["firstname"];?> <?php echo $row_attendance["lastname"];?></td>
-	   <td><img src="<?php echo $row_attendance["photo"];?>" width="40" class="img-circle"> </td>
-	   <td><?php echo $row_attendance["position"];?></td>
-	   <td><?php echo $row_attendance["gross_salary"];?></td>
-	   <td><?php
-			
-			if($row_attendance["attendance_type"] == 1) {
-				echo "Doctor";
-			}
-			else if($row_attendance["attendance_type"] == 2) {
-				echo "Nurse";
-			}
-			else {
-				echo "Employee";
-			}
-		
-		?></td>
-	   <td><?php echo $row_attendance["department_name"]; ?></td>
-		<td class="noprint">
-			<a href="attendance_edit.php?attendance_id=<?php echo $row_attendance["attendance_id"]; ?>">
-				<span class="glyphicon glyphicon-edit"></span>
-			</a>
-		</td>
-		<td class="noprint">
-			<a class="delete" href="attendance_delete.php?attendance_id=<?php echo $row_attendance["attendance_id"]; ?>">
-				<span class="glyphicon glyphicon-trash"></span>
-			</a>
-		</td>
+
+<form method="get" class="noprint">
 	
-	    
-  
-  </tr>
-  
-  <?php } while($row_attendance = mysqli_fetch_assoc($attendance));?>
+	<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+	
+	<div class="input-group">
+		<span class="input-group-addon">
+			Month: 
+		</span>
+		<select class="form-control" name="month">
+			<option value="1">January</option>
+			<option value="2">February</option>
+			<option value="3">March</option>
+			<option value="4">April</option>
+			<option value="5">May</option>
+			<option value="6">June</option>
+			<option value="7">July</option>
+			<option value="8">Auguest</option>
+			<option value="9">September</option>
+			<option value="10">October</option>
+			<option value="11">November</option>
+			<option value="12">December</option>
+		</select>
+	</div>
+	
+	</div>
+	
+	<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+		
+		<div class="input-group">
+			<span class="input-group-addon">
+				Year:
+			</span>
+			<input value="<?php echo date("Y"); ?>" type="text" name="year" class="form-control">
+			<span class="input-group-btn">
+				<input type="submit" value="Show" class="btn btn-primary">
+			</span>
+		</div>
+		
+		
+		
+		
+	
+	</div>
+	
+</form>
+
+<table class="table table-striped">
+	<tr>
+		<th>ID</th>
+		<th>Staff Name</th>
+		<th>Photo</th>
+		<th>Position</th>
+		<th>Hours</th>
+		<th class="noprint">Detail</th>
+	</tr>
+
+	<?php do { ?>
+	<tr>
+		<td><?php echo $row_attendance["staff_id"]; ?></td>
+		<td><?php echo $row_attendance["firstname"]; ?> <?php echo $row_attendance["lastname"]; ?></td>
+		<td><img src="<?php echo $row_attendance["photo"]; ?>" width="40" class=""></td>
+		<td><?php echo $row_attendance["position"]; ?></td>
+		<td><?php
+				echo $row_attendance["totalhours"] != "" ? $row_attendance["totalhours"] : "0";
+			?> hrs </td>
+			
+		<td class="noprint">
+			<a href="attendance_detail.php?staff_id=<?php echo $row_attendance["staff_id"]; ?>&absent_year=<?php echo $year; ?>&absent_month=<?php echo $month; ?>">
+				<span class="glyphicon glyphicon-info-sign"></span>
+			</a>
+		</td>
+		
+	</tr>
+	<?php } while($row_attendance = mysqli_fetch_assoc($attendance)); ?>
 	
 </table>
 
 
-<?php require_once("footer_mis.php");?>
+<?php require_once("footer_mis.php"); ?>
