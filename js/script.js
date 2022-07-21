2893b1f8d35aa559bcdf413c8d80ca745ea99d3f
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-07-21 09:15:35 +0430

    add function

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
 $(document).ready(function() {
 	
-	$("a#print").click(function() {
-		window.print();
-	});
 	
-	
-	window.setTimeout(function() {
-		$(".alert").slideUp(500);
-	}, 5000);
-	
-	
-	$("a.delete").click(function() {
-		var result = window.confirm("Are you sure you want to delete?");
-		if(!result) {
-			event.preventDefault();
-		}
-	});
-	
-});
