7b33f95d406b84f87dd8bfb2c76208dcee351a9f
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-07-21 11:28:49 +0430

    add jequery code to show confirm window

--- js/script.js
+++ js/script.js
@@ -4,5 +4,15 @@ $(document).ready(function() {
 	  $(.alert)slideUp(500);
 	  }, 5000);
 	  
-	  
+	 $("a.delete").click(function() {
+	 var result= window.confirm("Are you sure you want to delete?");
+	 
+	 if (!result){
+	     event.preventDefault();
+		 
+	 
+	 }
+	 
+	 
+	 });
 });
