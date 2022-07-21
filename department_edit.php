274308d2f74a3b9cb64006e7817773939c364efe
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-07-21 10:44:10 +0430

    select department name via ID

--- department_edit.php
+++ department_edit.php
@@ -1,29 +1,30 @@
 <?php require_once("connection.php"); ?>
 <?php
+
+	$department_id = getValue($_GET["department_id"]);
+	
+	$department = mysqli_query($con, "SELECT * FROM department WHERE department_id = $department_id");
+	$row_department = mysqli_fetch_assoc($department);
+
 	
 	if(isset($_POST["department_name"])) {
 		$department_name = getValue($_POST["department_name"]);
 		
-		$result = mysqli_query($con, "INSERT INTO department VALUES (NULL, '$department_name')");
+		$result = mysqli_query($con, "UPDATE department SET department_name='$department_name' WHERE department_id = $department_id");
 		
 		if($result) {
-			header("location:department_list.php?add=done");
+			header("location:department_list.php?edit=done");
 		}
 		else {
-			header("location:department_add.php?error=notadd");
+			header("location:department_edit.php?error=notedit&department_id=$department_id");
 		}
 		
 	}
 	
 ?>
-
-
-<?php require_once("header.php");?>
-
-
+<?php require_once("header.php"); ?>
 
 <div class="col-lg-8 col-md-8 col-ms-8 col-xs-12 col-lg-offset-2 col-md-offset-2 col-sm-offset-2 col-xs-offset-0">
-
 
 <div class="panel panel-primary">
 	
@@ -32,7 +33,8 @@
 	</div>
 
 	<div class="panel-body">
-	<?php if(isset($_GET["error"])) { ?>
+	
+		<?php if(isset($_GET["error"])) { ?>
 			<div class="alert alert-danger">
 				Could not save changes!
 			</div>
@@ -44,17 +46,18 @@
 				<span class="input-group-addon">
 					Department Name:
 				</span>
-				<input type="text" class="form-control" name="department_name">
+				<input type="text" value="<?php echo $row_department["department_name"]; ?>" class="form-control" name="department_name">
 			</div>
 			
 			<input type="submit" class="btn btn-primary" value="Save Changes">
 			
 		</form>
-		</div>
 		
 	</div>
 
 </div>
-<?php require_once("footer_mis.php");?>
 
+</div>
+
+<?php require_once("footer_mis.php"); ?>
 
