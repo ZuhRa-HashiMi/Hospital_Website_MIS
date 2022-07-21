ca09ad857324c8c99822bf007b28ce5791c37b19
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-07-21 14:13:38 +0430

    bring some changes

--- header.php
+++ header.php
@@ -11,9 +11,11 @@
 <title>HMIS</title>
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 <link href="css/bootstrap-theme.min.css" rel="stylesheet" type="text/css" />
 <link href="css/style.css" rel="stylesheet" type="text/css" />
+
 <script type="text/javascript" src="js/jquery.min.js"></script>
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/script.js"></script>
