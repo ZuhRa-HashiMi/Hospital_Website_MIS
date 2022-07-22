ecea556ad39b983a0d727c55b983355184b1f3b0
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-07-23 00:29:30 +0430

    add click event to handel print button

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
 	
-	window.setTimout(function()) {
-	  $(.alert)slideUp(500);
-	  }, 5000);
-	  
-	 $("a.delete").click(function() {
-	 var result= window.confirm("Are you sure you want to delete?");
-	 
-	 if (!result){
-	     event.preventDefault();
-		 
-	 
-	 }
-	 
-	 
-	 });
+	$("a#print").click(function() {
+		window.print();
+	});
+	
+	
+	window.setTimeout(function() {
+		$(".alert").slideUp(500);
+	}, 5000);
+	
+	
+	$("a.delete").click(function() {
+		var result = window.confirm("Are you sure you want to delete?");
+		if(!result) {
+			event.preventDefault();
+		}
+	});
+	
 });
