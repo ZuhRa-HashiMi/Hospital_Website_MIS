aedcc5a6a68884c90725f940a9ead2107d9bdddd
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-07-23 20:10:03 +0430

    add english language menu

--- local/en.php
+++ local/en.php
@@ -1,20 +1,10 @@
 <?php
 
-     $menu_home="Home":;
-	 $menu_gallery="Gallery":;
-	 $menu_services="Services":;
-	  $menu_News="News":;
-	  $menu_aboutus="About Us":;
-	   $menu_contactus="Contact Us":;
-	 
-
-
-
-
-
-
-
-
-
+	$menu_home = "Home";
+	$menu_gallery = "Gallery";
+	$menu_services = "Services";
+	$menu_news = "News";
+	$menu_aboutus = "About Us";
+	$menu_contactus = "Contact Us";
 
 ?>
