a7ad69be546669c65459ca94677c2ecc27353ff0
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-07-23 18:24:14 +0430

    add some changes to this file

--- staff_list.php
+++ staff_list.php
@@ -86,9 +86,11 @@
   <tr>
        <td><?php echo $row_staff["staff_id"];?></td>
        <td><?php echo $row_staff["firstname"];?> <?php echo $row_staff["lastname"];?></td>
+	     <td><?php echo $row_staff["position"];?></td>
+		 <td><?php echo $row_staff["gross_salary"];?></td>
 	   <td><img src="<?php echo $row_staff["photo"];?>" width="40" class="img-circle"> </td>
-	   <td><?php echo $row_staff["position"];?></td>
-	   <td><?php echo $row_staff["gross_salary"];?></td>
+	
+	
 	   <td><?php
 			
 			if($row_staff["staff_type"] == 1) {
